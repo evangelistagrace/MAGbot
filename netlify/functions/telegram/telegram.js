66a8d59b20f86ac2776d78cf6a0b669eab6c281b
Grace <evangrace98@gmail.com>
2022-08-08 00:18:36 +0800

    configure serverless function

--- netlify/functions/telegram/telegram.js
+++ netlify/functions/telegram/telegram.js
@@ -11,57 +11,21 @@ access_token_req_data = `grant_type=client_credentials&client_id=${process.env.c
 exports.handler = async (event, context) => {
     try {
         let {case_id, case_title, case_category, case_type, case_type_detail} = JSON.parse(event.body)
-
-        /* GET ACCESS TOKEN */
-        let response = await axios.post('https://cloud.uipath.com/identity_/connect/token', access_token_req_data, {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
+    
+        const res = await axios.post(`${url}${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,{
+            chat_id: process.env.TELEGRAM_CHAT_ID,
+            text: `[NEW CASE]\nCase#${case_id}: ${case_title}\nType: ${case_type}\nCategory: ${case_category} [${case_type_detail}]`
+    
+        }).then((response) => {
+          let date = new Date()
+            console.log("process executed at ", date) 
         })
-
-        access_token = response.data.access_token
-
-        console.log('access_token: ', access_token)
-        
-        /* GET RELEASE KEY */
-        response = await axios.get(`${base_url}/odata/Releases?$filter=ProcessKey eq \'${process_name}\'`, {
-            headers: {
-                'Authorization': `Bearer ${access_token}`,
-                'X-UIPATH-OrganizationUnitId': process.env.org_unit_id
-            }
-        })
-
-        release_key = response.data.value[0]['Key']
-
-        console.log('release_key: ', release_key)
-
-        /* START JOB */
-        response = await axios.post(`${base_url}/odata/Jobs/UiPath.Server.Configuration.OData.StartJobs`, {
-            "startInfo": {
-                "ReleaseKey": release_key,
-                "Strategy": "ModernJobsCount",
-                "JobsCount": "1",
-                "InputArguments": `[NEW CASE]\nCase#${case_id}: ${case_title}\nType: ${case_type}\nCategory: ${case_category} [${case_type_detail}]`
-            } 
-        }, {
-            headers: {
-                'Authorization': `Bearer ${access_token}`,
-                'X-UIPATH-OrganizationUnitId': process.env.org_unit_id,
-                'X-UIPATH-Tenant': process.env.tenant_name
-            }
-        })
-
-        if (response.status == '201' && response.statusText=='Created') {
-            console.log('Job started!')
-        }
-
         return {
-            statusCode: 204,
-            body: JSON.stringify({ message: "OK" }),
-        }
-
-    } catch (err) {
-        return { statusCode: 500, body: err.toString() }
-    }
+          statusCode: 204,
+          body: JSON.stringify({ message: "OK" }),
+        };
+      } catch (err) {
+        return { statusCode: 500, body: err.toString() };
+      }
 }
 
