a69d851d51f903929ac6dfe6d1781acd1921b391
Grace <evangrace98@gmail.com>
2022-08-08 00:34:10 +0800

    commit

--- netlify/functions/telegram/telegram.js
+++ netlify/functions/telegram/telegram.js
@@ -1,15 +1,15 @@
 const axios = require("axios").default;
 require('dotenv').config()
 
-const process_name = 'Send.Telegram.Notifications',
-base_url = `https://cloud.uipath.com/${process.env.org_name}/${process.env.tenant_name}/orchestrator_`
-
-let access_token='',
-release_key='',
-access_token_req_data = `grant_type=client_credentials&client_id=${process.env.client_id}&client_secret=${process.env.client_secret}&scope=OR.Execution+OR.Folders+OR.Jobs+OR.Machines+OR.Queues+OR.Robots`;
-
 exports.handler = async (event, context) => {
     try {
+        const process_name = 'Send.Telegram.Notifications',
+            base_url = `https://cloud.uipath.com/${process.env.org_name}/${process.env.tenant_name}/orchestrator_`
+
+        let access_token='',
+            release_key='',
+            access_token_req_data = `grant_type=client_credentials&client_id=${process.env.client_id}&client_secret=${process.env.client_secret}&scope=OR.Execution+OR.Folders+OR.Jobs+OR.Machines+OR.Queues+OR.Robots`
+
         let {case_id, case_title, case_category, case_type, case_type_detail} = JSON.parse(event.body)
 
         /* GET ACCESS TOKEN */
@@ -34,7 +34,7 @@ exports.handler = async (event, context) => {
         release_key = response.data.value[0]['Key']
 
         console.log('release_key: ', release_key)
-        
+
         return {
             statusCode: 204,
             body: JSON.stringify({ message: "OK"}),
