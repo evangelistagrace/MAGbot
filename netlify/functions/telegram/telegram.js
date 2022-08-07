07c2cf54d1e14cb699dc433b1e52f16b8cc43e47
Grace <evangrace98@gmail.com>
2022-08-08 00:27:27 +0800

    commit

--- netlify/functions/telegram/telegram.js
+++ netlify/functions/telegram/telegram.js
@@ -8,26 +8,29 @@ let access_token='',
 release_key='',
 access_token_req_data = `grant_type=client_credentials&client_id=${process.env.client_id}&client_secret=${process.env.client_secret}&scope=OR.Execution+OR.Folders+OR.Jobs+OR.Machines+OR.Queues+OR.Robots`;
 
-
-const url = 'https://api.telegram.org/bot';
 exports.handler = async (event, context) => {
     try {
         let {case_id, case_title, case_category, case_type, case_type_detail} = JSON.parse(event.body)
-    
-        const res = await axios.post(`${url}${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`,{
-            chat_id: process.env.TELEGRAM_CHAT_ID,
-            text: `[NEW CASE]\nCase#${case_id}: ${case_title}\nType: ${case_type}\nCategory: ${case_category} [${case_type_detail}]`
-    
-        }).then((response) => {
-          let date = new Date()
-            console.log("process executed at ", date) 
+
+        /* GET ACCESS TOKEN */
+        let response = await axios.post('https://cloud.uipath.com/identity_/connect/token', access_token_req_data, {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
         })
+
+        access_token = response.data.access_token
+
+        console.log('access_token: ', access_token)
+        
+        
         return {
-          statusCode: 204,
-          body: JSON.stringify({ message: "OK" }),
-        };
-      } catch (err) {
-        return { statusCode: 500, body: err.toString() };
-      }
+            statusCode: 204,
+            body: JSON.stringify({ message: "OK"}),
+        }
+
+    } catch (err) {
+        return { statusCode: 500, body: err.toString() }
+    }
 }
 
